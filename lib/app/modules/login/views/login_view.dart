eb288c80e176368dc76a81d7ff1a6789a118f94b
Satria Wahyu Kurniawan <satriawahyuk1909@gmail.com>
2022-11-10 06:33:45 +0700

    commit_pertama

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
