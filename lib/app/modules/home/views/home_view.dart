086bb62db229300aeb9db1188dbc51d6d9044022
Satria Wahyu Kurniawan <satriawahyuk1909@gmail.com>
2022-11-10 12:34:07 +0700

    commit home_view.dart, sideBar.dart & header

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:task_management_app/app/routes/app_pages.dart';
+import 'package:task_management_app/app/utils/widget/header.dart';
 import 'package:task_management_app/app/utils/widget/sideBar.dart';
 
 import '../controllers/home_controller.dart';
@@ -13,21 +14,32 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: SideBar(),
-          ),
-          Expanded(
-            flex: 15,
-            child: Container(
-              color: Colors.white,
+        backgroundColor: Colors.grey[100],
+        body: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: SideBar(),
             ),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              flex: 15,
+              child: Column(
+                children: [
+                  header(),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(50),
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
