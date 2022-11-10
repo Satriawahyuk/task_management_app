74b17a63b5ac28434a40644808e7b9b60e0176f6
Satria Wahyu Kurniawan <satriawahyuk1909@gmail.com>
2022-11-10 10:32:08 +0700

    commit home_view.dart & sideBar.dart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+import 'package:task_management_app/app/utils/widget/sideBar.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,15 +13,20 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Colors.grey[100],
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: SideBar(),
+          ),
+          Expanded(
+            flex: 15,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
